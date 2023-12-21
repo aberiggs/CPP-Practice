8945055b4434fa13da19dc160ffef1e4f6fb7dd3
Sprocket Riggs <sprocket710@gmail.com>
2023-12-20 17:44:07 -0800

    AoC Day 2 - Part 1

---
+++ AoC-2023/Day-2/part_one.cpp
@@ -0,0 +1,70 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+bool impossibleInLine(std::string line);
+bool impossibleInSection(std::string section);
+
+
+int main() {
+    std::string filename { "input.txt" };
+    std::ifstream input { filename };
+
+    if (!input) {
+        std::cout << "File '" << filename << "' failed to open.\n";
+        return 1;
+    }
+
+    int idSum {};
+    int gameNum { 1 };
+    while (input) {
+        std::string line {};
+        std::getline(input, line);
+
+        if (line.length() > 0 && !impossibleInLine(line))
+            idSum += gameNum;
+
+        ++gameNum;
+    }
+
+    std::cout << "Sum of all IDs of possible games: " << idSum << "\n"; 
+
+    return 0;
+}
+
+bool impossibleInLine(std::string line) {
+    std::cout << line << "\n";
+
+    int startPos { static_cast<int>(line.find(":"))};
+    int endPos { static_cast<int>(std::min(line.find(","), line.find(";"))) };
+    
+    while (startPos < line.length()) {
+        if (impossibleInSection(line.substr(startPos+2, endPos-startPos))) 
+            return true;
+
+        startPos = endPos;
+        endPos = std::min(line.find(",", endPos+1), line.find(";", endPos+1));
+    }
+
+    return false;
+}
+
+bool impossibleInSection(std::string section) {
+    int redMax { 12 };
+    int greenMax { 13 };
+    int blueMax { 14 };
+    std::cout << "Section: " << section << "\n";
+    int val { std::stoi(section) };
+
+    if ((section.find("red") != std::string::npos) && (val > redMax))
+        return true;
+    else if ((section.find("green") != std::string::npos) && (val > greenMax))
+        return true;
+    else if ((section.find("blue") != std::string::npos) && (val > blueMax))
+        return true;
+
+
+    return false;
+}
+
